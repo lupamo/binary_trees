208b74205ea520e2d3e93122f56907138ef5dc54
lupamo <arnoldlupamo@gmail.com>
2024-02-29 12:38:55 +0300

    node new reverted

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,11 +14,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	new_node = malloc(sizeof(binary_tree_t));
+	
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new_node = malloc(sizeof(binary_tree_t));
 
 
 	new_node->n = value;
