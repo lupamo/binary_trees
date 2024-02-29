fac0ee32a4d03c218b32b6021e0329e8528d3a80
lupamo <arnoldlupamo@gmail.com>
2024-02-29 12:35:58 +0300

    node new

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,7 +10,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 	{
 		return (NULL);
@@ -19,6 +18,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	new_node = malloc(sizeof(binary_tree_t));
+
 
 	new_node->n = value;
 	new_node->parent = parent;
