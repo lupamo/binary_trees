d1dfe6547f95357f667a816bbc0d419091b55321
lupamo <arnoldlupamo@gmail.com>
2024-02-29 12:33:03 +0300

    betty error spaces

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,18 +13,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = parent->right;
-	
+
 	if (parent->right != NULL)
 	{
 		parent->right->parent = new_node;
