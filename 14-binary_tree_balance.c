d40754929b683edadb939d262e834e9e7e329e44
lupamo <arnoldlupamo@gmail.com>
2024-03-01 06:00:05 +0300

    uncle

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,8 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - function to measure balane factor of a binary tree
+ * binary_tree_balance - function to measure balance factor of a binary tree
  * @tree: root node pointer
  * Return: 0 if root node is NULL
 */
 int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    return (binary_tree_hei)    
+}
