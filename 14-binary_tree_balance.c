8a17e49b8dcbb9dab38b9cc7efeba5f922af0b28
lupamo <arnoldlupamo@gmail.com>
2024-03-01 05:54:00 +0300

    sibllings

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - function to measure balane factor of a binary tree
+ * @tree: root node pointer
+ * Return: 0 if root node is NULL
+*/
+int binary_tree_balance(const binary_tree_t *tree)
