a83c69488b25af97f185654ba1247ab0aeb61d0a
lupamo <arnoldlupamo@gmail.com>
2024-03-01 05:17:42 +0300

    tree size output

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,18 +8,9 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t level;
-
-	level = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	if (tree->parent != NULL)
-	{
-		level = 1 + binary_tree_size(tree->parent);
-	}
-
-	return (level);
+	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);	
 }
